e457a04fd6aadf257d955c1f559c59f68ae7f15e
Faizan Ali <imfaizanali22@gmail.com>
2026-06-10 16:07:45 +0500

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,118 @@
+# Task 1
+
+a = 9
+b = 5
+
+if a > b:
+    print("a is greater than b")
+
+# Task 2
+
+c = "Faizan"
+d = "Faizan"
+
+if c == d:
+    print("Both name matched")
+
+# Task 3
+
+num1 = 10
+num2 = 15
+
+if num1 < num2:
+    print("num1 is less than num2")
+
+# Task 4
+
+age = 18
+
+if age >= 18:
+    print("You are eligible to vote")
+
+# Task 5
+
+Marks = int(input("Enter your marks: "))
+
+if Marks >= 50:
+    print("You passed the exam")
+else :   print("You failed the exam")
+
+# Task 6
+
+e = 10
+f = 20
+
+if e < f:
+    print("e is less than f")
+else:   print("f is greater than or equal to e")
+
+# Task 7
+
+variable = ("")
+
+if variable == "":
+    print("Variable is empty")
+else:
+    print("Variable is not empty")
+
+# Task 8
+
+Name = "Faizan Ali"
+
+if Name == "Faizan Ali":
+    print("Name is Available")
+
+# Task 9
+
+x = 0
+
+if x:
+    print("Truthy")
+else:
+    print("Falsy")  
+
+# Task 10
+
+y = 5
+
+if y:
+    print("Truthy")
+else:
+    print("Falsy") 
+
+# Task 11
+
+English = 85
+
+if English >= 80:
+    print("You got A Grade")
+else:   print("You need improvement") 
+
+Maths = 90
+
+if Maths >= 80:
+    print("You got A Grade")
+else:   print("You need improvement")
+
+Urdu = 65
+
+if Urdu >= 80:
+    print("You got a passing grade")
+else:   print("You need improvement")
+
+# Task 12
+
+Username = "Ali"
+password = "123"
+
+login_username = input("Enter your username: ")
+login_password = input("Enter your password: ")
+
+if login_username == Username :
+    print("Username is correct")
+else:   print("Username is incorrect")
+
+if login_password == password:
+    print("Password is correct")
+else:   print("Password is incorrect")
+
