19a4f2ee19010666fc73587f170427bf93b21e74
Faizan Ali <imfaizanali22@gmail.com>
2026-06-13 11:55:31 +0500

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,60 @@
+# Task 1
+name = "Fazian Ali"
+age = 15
+city = "Faisalabad"
+
+print(name)
+print(age)
+print(city)
+
+
+# Task 2
+name = "Ali"
+age = 14
+isStudent = True
+
+print(type(name))
+print(type(age))
+print(type(isStudent))
+
+
+# Task 3
+marks = "95"
+
+marks_int = int(marks)
+print(marks_int)
+print(type(marks_int))
+
+
+# Task 4
+phoneNumber = 123456789
+
+phone_str = str(phoneNumber)
+print(phone_str)
+print(type(phone_str))
+
+
+# Task 5
+userName = "Ahmed"
+
+user_bool = bool(userName)
+print(user_bool)
+print(type(user_bool))
+
+# Task 6
+name = "Ali"
+age = 18
+favorite_color = "Blue"
+isStudent = True
+
+print("Name:", name)
+print("Age:", age)
+print("Favorite Color:", favorite_color)
+print("Student:", isStudent)
+
+
+# Bonus Task
+num = "25"
+
+num_int = int(num)
+print(num_int, "+ 50 =", num_int + 50)
